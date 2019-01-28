63b520252403cc3bf4a03c0217643ac82e1a68fa
dutraneto <dudutraneto@gmail.com>
2019-01-28 11:54:28 -0500

    update

--- src/main.js
+++ src/main.js
@@ -7,8 +7,11 @@ const convertBRL = require('./convertBRL');
 program
     .version(pkg.version)
     .description('Convert Brazilian Currency to Canadian Currency')
-    .option('-C, --currency <currency>', 'currency to be converted <not implemented yet> (default: BRL)')
+    .option(
+        '-C, --currency <currency>',
+        'currency to be converted <not implemented yet> (default: BRL)'
+    )
     .option('-A, --amount <amount>', 'Value in BRL to convert (default: 1)')
-    .parse(process.argv)
+    .parse(process.argv);
 
 convertBRL(program.currency, program.amount);
